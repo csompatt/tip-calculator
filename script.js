20ca1b59bcbe80539c6e2f08028a82046db00de7
csompatt <csompatt@icloud.com>
2022-10-30 08:18:46 +0100

    Finished design and bug fixed
    Finished CSS design and JavaScript bugs fixed

--- script.js
+++ script.js
@@ -1,52 +1,93 @@
-const inputs = document.querySelectorAll('input');
-const radios = document.querySelectorAll('input[name="tip-amount"]');
+const inputs = document.querySelectorAll('input'); // EVERY INPUT
+const radios = document.querySelectorAll('input[name="tip-amount"]'); // EVERY RADIO BUTTON
 
-const billInput = document.getElementById('bill-input');
-const numberOfPeople = document.getElementById('number-of-people');
-const tipAmountPerPerson = document.querySelector('.tip-person');
-const totalAmountPerPerson = document.querySelector('.total-person');
-const customTipSelector = document.getElementById('custom-tip');
-const customTipAmount = document.getElementById('custom-tip-amount');
-const resetButton = document.getElementById('reset-button');
+const pageBody = document.getElementsByName('body'); // BODY ELEMENT
+const billInput = document.getElementById('bill-input'); // BILL AMOUNT INPUT ELEMENT
+const numberOfPeopleInput = document.getElementById('number-of-people'); // NUMBER OF PEOPLE INPUT ELEMENT
+const tipAmountPerPerson = document.querySelector('.tip-person'); // TIP/PERSON H4 ELEMENT
+const totalAmountPerPerson = document.querySelector('.total-person'); // TOTAL/PERSON H4 ELEMENT
+const customTipRadio = document.getElementById('custom-tip'); // CUSTOM TIP RADIO BUTTON
+const customTipInput = document.getElementById('custom-tip-input'); // CUSTOM TIP INPUT ELEMENT
+const customTipLabel = document.querySelector('.custom-tip-label'); // CUSTOM TIP LABEL ELEMENT
+const resetButton = document.getElementById('reset-button'); // RESET BUTTON
+const errorMessage = document.querySelector('.error-message'); // ERROR MESSAGE IF NUMBER OF PEOPLE IS 0 OR LESS (H2)
+
+let numberOfPeople = 1; // NUMBER OF THE PEOPLE
+
+// ***** CALCULATING FUNCTIONS *****
 
 function roundTwoDecimals(number) {
   return Math.round(number * 100) / 100;
 }
 
 function calculateTip(selectedTip) {
-  return roundTwoDecimals((billInput.value * selectedTip.value) / numberOfPeople.value);
+  return roundTwoDecimals((billInput.value * selectedTip.value) / numberOfPeople);
 }
 
 function calculateTotalPerPerson(selectedTip) {
-  return calculateTip(selectedTip) + billInput.value / numberOfPeople.value;
+  return roundTwoDecimals(calculateTip(selectedTip) + billInput.value / numberOfPeople);
 }
 
 function calculateCustomTip() {
-  return roundTwoDecimals((billInput.value * customTipAmount.value) / 100 / numberOfPeople.value);
+  return roundTwoDecimals((billInput.value * customTipInput.value) / 100 / numberOfPeople);
 }
 
 function calculateCustomTotalPerPerson() {
-  return calculateCustomTip() + billInput.value / numberOfPeople.value;
+  return roundTwoDecimals(calculateCustomTip() + billInput.value / numberOfPeople);
 }
 
+// ***** CHECK THE NUMBER OF PEOPLE, IF 0 OR LESS SHOW ERROR MSG *****
+
+numberOfPeopleInput.addEventListener('input', function () {
+  numberOfPeople = numberOfPeopleInput.value;
+
+  if (numberOfPeople <= 0) {
+    errorMessage.style.display = 'inline-block';
+    numberOfPeopleInput.classList.add('error-message-border');
+  } else {
+    numberOfPeopleInput.classList.remove('error-message-border');
+    errorMessage.style.display = 'none';
+  }
+});
+
+// ***** CHECK WHICH TIP SELECTED AND PRINT THE RESULT ******
+
 inputs.forEach(function (elem) {
+  let selectedTip;
+
   elem.addEventListener('input', function printTip() {
-    if (customTipSelector.checked) {
-      tipAmountPerPerson.textContent = calculateCustomTip();
-      totalAmountPerPerson.textContent = calculateCustomTotalPerPerson();
+    if (customTipRadio.checked) {
+      tipAmountPerPerson.textContent = '€' + calculateCustomTip();
+      totalAmountPerPerson.textContent = '€' + calculateCustomTotalPerPerson();
+
+      customTipLabel.style.display = 'none';
+      customTipInput.style.display = 'block';
     } else {
-      const selectedTip = document.querySelector('input[name="tip-amount"]:checked');
-      tipAmountPerPerson.textContent = calculateTip(selectedTip);
-      totalAmountPerPerson.textContent = calculateTotalPerPerson(selectedTip);
+      selectedTip = document.querySelector('input[name="tip-amount"]:checked');
+      customTipRadio.checked = false;
+      customTipInput.style.display = 'none';
+      customTipLabel.style.display = 'flex';
+
+      if (selectedTip) {
+        tipAmountPerPerson.textContent = '€' + calculateTip(selectedTip);
+        totalAmountPerPerson.textContent = '€' + calculateTotalPerPerson(selectedTip);
+      }
     }
   });
 });
 
+// ***** RESET *****
+
 resetButton.addEventListener('click', function () {
-  inputs.forEach(function (elem) {
-    elem.value = 0;
+  numberOfPeople = 1;
+  billInput.value = '';
+  tipAmountPerPerson.textContent = '0.00 €';
+  totalAmountPerPerson.textContent = '0.00 €';
+  customTipInput.style.display = 'none';
+  customTipLabel.style.display = 'flex';
+  customTipInput.value = '';
+
+  radios.forEach(function (elem) {
+    elem.checked = false;
   });
-  numberOfPeople.value = 1;
-  tipAmountPerPerson.textContent = '';
-  totalAmountPerPerson.textContent = '';
 });
